73e87931eea6ba994d9edb30653b027ffcc501c9
Constellation <utatane.tea@gmail.com>
2012-11-10 01:17:12 +0900

    Add XenSource vendored device model
    ref #1

--- tools/qemu-xen-traditional/hw/quadro6000.c
+++ tools/qemu-xen-traditional/hw/quadro6000.c
@@ -28,7 +28,54 @@
 #include "irq.h"
 #include "quadro6000.h"
 
+typedef struct quadro6000_state {
+  PCIDevice  pci_dev;
+} quadro6000_state_t;
+
+struct pci_config_header {
+    uint16_t vendor_id;
+    uint16_t device_id;
+    uint16_t command;
+    uint16_t status;
+    uint8_t  revision;
+    uint8_t  api;
+    uint8_t  subclass;
+    uint8_t  class;
+    uint8_t  cache_line_size; /* Units of 32 bit words */
+    uint8_t  latency_timer; /* In units of bus cycles */
+    uint8_t  header_type; /* Should be 0 */
+    uint8_t  bist; /* Built in self test */
+    uint32_t base_address_regs[6];
+    uint32_t reserved1;
+    uint16_t subsystem_vendor_id;
+    uint16_t subsystem_id;
+    uint32_t rom_addr;
+    uint32_t reserved3;
+    uint32_t reserved4;
+    uint8_t  interrupt_line;
+    uint8_t  interrupt_pin;
+    uint8_t  min_gnt;
+    uint8_t  max_lat;
+};
 
 void pci_quadro6000_init(PCIBus* bus) {
-    printf("Register Quadro6000 device model\n");
+    quadro6000_state_t* state;
+    struct pci_config_header* pch;
+    int instance;
+
+    state = (quadro6000_state_t*)pci_register_device(bus, "quadro6000", sizeof(quadro6000_state_t), -1, NULL, NULL);
+    pch = (struct pci_config_header *)state->pci_dev.config;
+    pch->vendor_id = 0x5853;
+    pch->device_id = 0x0003;
+    pch->command = 3; /* IO and memory access */
+    pch->revision = 1;
+    pch->api = 0;
+    pch->subclass = 0x80; /* Other */
+    pch->class = 0xff; /* Unclassified device class */
+    pch->header_type = 0;
+    pch->interrupt_pin = 1;
+
+    instance = pci_bus_num(bus) << 8 | state->pci_dev.devfn;
+
+    printf("Register Quadro6000 device model: %x\n", instance);
 }
