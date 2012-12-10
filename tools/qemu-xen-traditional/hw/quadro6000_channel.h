c478500075898f67fda9ac677904a04e27e61302
Constellation <utatane.tea@gmail.com>
2012-12-10 18:27:00 +0900

    Add offset

--- tools/qemu-xen-traditional/hw/quadro6000_channel.h
+++ tools/qemu-xen-traditional/hw/quadro6000_channel.h
@@ -1,7 +1,9 @@
 #ifndef HW_QUADRO6000_CHANNEL_H_
 #define HW_QUADRO6000_CHANNEL_H_
+#include <stddef.h>
 
 typedef struct quadro6000_channel {
+    size_t offset;
 } quadro6000_channel_t;
 
 #endif  // HW_QUADRO6000_CHANNEL_H_
