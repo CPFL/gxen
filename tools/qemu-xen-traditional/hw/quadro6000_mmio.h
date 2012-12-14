98566240d4276bfad0ef01c3e78df8fbeae58823
Constellation <utatane.tea@gmail.com>
2012-12-14 17:27:21 +0900

    Add real device redirection

---
+++ tools/qemu-xen-traditional/hw/quadro6000_mmio.h
@@ -0,0 +1,8 @@
+#ifndef HW_QUADRO6000_MMIO_H_
+#define HW_QUADRO6000_MMIO_H_
+#include "quadro6000.h"
+
+void quadro6000_init_mmio(quadro6000_state_t* state);
+
+#endif  // HW_QUADRO6000_MMIO_H_
+/* vim: set sw=4 ts=4 et tw=80 : */
