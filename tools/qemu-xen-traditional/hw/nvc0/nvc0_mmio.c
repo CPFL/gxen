be9546f5fe557ea5ee995e38d6c9f0e45dbbbb8a
Constellation <utatane.tea@gmail.com>
2013-01-01 17:28:22 +0900

    Insert debug printf into BAR1 access

--- tools/qemu-xen-traditional/hw/nvc0/nvc0_mmio.c
+++ tools/qemu-xen-traditional/hw/nvc0/nvc0_mmio.c
@@ -357,42 +357,54 @@ static void nvc0_init_bar1(nvc0_state_t* state) {
 static uint32_t nvc0_mmio_bar1_readb(void *opaque, target_phys_addr_t addr) {
     nvc0_state_t* state = (nvc0_state_t*)(opaque);
     const target_phys_addr_t offset = addr - state->bar[1].addr;
-    // return read8(state->bar[1].space, offset);
+    if (state->log) {
+        NVC0_PRINTF("read 0x%X\n", offset);
+    }
     return read8(state->bar[1].real, offset);
 }
 
 static uint32_t nvc0_mmio_bar1_readw(void *opaque, target_phys_addr_t addr) {
     nvc0_state_t* state = (nvc0_state_t*)(opaque);
     const target_phys_addr_t offset = addr - state->bar[1].addr;
-    // return read16(state->bar[1].space, offset);
+    if (state->log) {
+        NVC0_PRINTF("read 0x%X\n", offset);
+    }
     return read16(state->bar[1].real, offset);
 }
 
 static uint32_t nvc0_mmio_bar1_readd(void *opaque, target_phys_addr_t addr) {
     nvc0_state_t* state = (nvc0_state_t*)(opaque);
     const target_phys_addr_t offset = addr - state->bar[1].addr;
-    // return read32(state->bar[1].space, offset);
+    if (state->log) {
+        NVC0_PRINTF("read 0x%X\n", offset);
+    }
     return read32(state->bar[1].real, offset);
 }
 
 static void nvc0_mmio_bar1_writeb(void *opaque, target_phys_addr_t addr, uint32_t val) {
     nvc0_state_t* state = (nvc0_state_t*)(opaque);
     const target_phys_addr_t offset = addr - state->bar[1].addr;
-    // write8(state->bar[1].space, offset, val);
+    if (state->log) {
+        NVC0_PRINTF("write 0x%X <= 0x%X\n", offset, val);
+    }
     write8(state->bar[1].real, offset, val);
 }
 
 static void nvc0_mmio_bar1_writew(void *opaque, target_phys_addr_t addr, uint32_t val) {
     nvc0_state_t* state = (nvc0_state_t*)(opaque);
     const target_phys_addr_t offset = addr - state->bar[1].addr;
-    // write16(state->bar[1].space, offset, val);
+    if (state->log) {
+        NVC0_PRINTF("write 0x%X <= 0x%X\n", offset, val);
+    }
     write16(state->bar[1].real, offset, val);
 }
 
 static void nvc0_mmio_bar1_writed(void *opaque, target_phys_addr_t addr, uint32_t val) {
     nvc0_state_t* state = (nvc0_state_t*)(opaque);
     const target_phys_addr_t offset = addr - state->bar[1].addr;
-    // write32(state->bar[1].space, offset, val);
+    if (state->log) {
+        NVC0_PRINTF("write 0x%X <= 0x%X\n", offset, val);
+    }
     write32(state->bar[1].real, offset, val);
 }
 
