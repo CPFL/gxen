8f039a83c4ac055c18d3bfbfc8412d92e433b3e1
Constellation <utatane.tea@gmail.com>
2013-02-11 13:43:19 +0900

    Cleanup warnings and create NVC0_STATIC_ASSERT

---
+++ tools/qemu-xen-traditional/hw/nvc0/nvc0_inttypes.h
@@ -0,0 +1,8 @@
+#ifndef HW_NVC0_NVC0_INTTYPES_H_
+#define HW_NVC0_NVC0_INTTYPES_H_
+
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+
+#endif  // HW_NVC0_NVC0_INTTYPES_H_
+/* vim: set sw=4 ts=4 et tw=80 : */
---
+++ tools/qemu-xen-traditional/hw/nvc0/nvc0_static_assert.h
@@ -0,0 +1,23 @@
+#ifndef HW_NVC0_NVC0_STATIC_ASSERT_H_
+#define HW_NVC0_NVC0_STATIC_ASSERT_H_
+
+// http://stackoverflow.com/questions/174356/ways-to-assert-expressions-at-build-time-in-c
+#ifdef __GNUC__
+
+    #define NVC0_STATIC_ASSERT_HELPER(expr, msg) \
+        (!!sizeof \
+            (struct { unsigned int NVC0_STATIC_ASSERTION__##msg: (expr) ? 1 : -1; }))
+
+    #define NVC0_STATIC_ASSERT(expr, msg) \
+        extern int (*assert_function__(void)) [NVC0_STATIC_ASSERT_HELPER(expr, msg)]
+
+#else
+
+    #define NVC0_STATIC_ASSERT(expr, msg) \
+        extern char NVC0_STATIC_ASSERTION__##msg[1]; \
+        extern char NVC0_STATIC_ASSERTION__##msg[(expr)?1:2]
+
+#endif  /* #ifdef __GNUC__ */
+
+#endif  // HW_NVC0_NVC0_STATIC_ASSERT_H_
+/* vim: set sw=4 ts=4 et tw=80 : */
