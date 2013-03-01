a595c7bbd865faf8e7cb59b1bddc0de7fdf95238
Constellation <utatane.tea@gmail.com>
2013-03-01 22:52:40 +0900

    Add utilities from iv project

--- tools/qemu-xen-traditional/hw/nvc0/nvc0_static_assert.h
+++ tools/qemu-xen-traditional/hw/nvc0/nvc0_static_assert.h
@@ -1,11 +1,14 @@
 #ifndef HW_NVC0_NVC0_STATIC_ASSERT_H_
 #define HW_NVC0_NVC0_STATIC_ASSERT_H_
 
+#define NVC0_JOIN2(x, y) x##y
+#define NVC0_JOIN(x, y) NVC0_JOIN2(x, y)
+
 // http://stackoverflow.com/questions/174356/ways-to-assert-expressions-at-build-time-in-c
 #if defined(__GNUC__) && !defined(__cplusplus)
     #define NVC0_STATIC_ASSERT_HELPER(expr, msg) \
         (!!sizeof \
-            (struct { unsigned int NVC0_STATIC_ASSERTION__##msg: (expr) ? 1 : -1; }))
+            (struct { unsigned int NVC0_JOIN(NVC0_STATIC_ASSERTION__##msg##__, __LINE__) : (expr) ? 1 : -1; }))
 
     #define NVC0_STATIC_ASSERT(expr, msg) \
         extern int (*assert_function__(void)) [NVC0_STATIC_ASSERT_HELPER(expr, msg)]
@@ -13,8 +16,7 @@
 #else
 
     #define NVC0_STATIC_ASSERT(expr, msg) \
-        extern char NVC0_STATIC_ASSERTION__##msg[1]; \
-        extern char NVC0_STATIC_ASSERTION__##msg[(expr)?1:2]
+        typedef char NVC0_JOIN(NVC0_STATIC_ASSERTION__##msg##__, __LINE__)[(!!(expr))*2-1]
 
 #endif  /* #ifdef __GNUC__ */
 
