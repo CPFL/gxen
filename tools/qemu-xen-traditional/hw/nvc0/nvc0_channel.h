42814a536b12dedcae175b6f0fc244846183f051
Constellation <utatane.tea@gmail.com>
2013-03-04 02:46:53 +0900

    Implement C++ based nvc0 channel

--- tools/qemu-xen-traditional/hw/nvc0/nvc0_channel.h
+++ tools/qemu-xen-traditional/hw/nvc0/nvc0_channel.h
@@ -1,19 +1,12 @@
 #ifndef HW_NVC0_NVC0_CHANNEL_H_
 #define HW_NVC0_NVC0_CHANNEL_H_
-#include <stddef.h>
+#include "nvc0.h"
+namespace nvc0 {
+class context;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-typedef struct nvc0_channel {
-    uint32_t status;
-    uint32_t id;
-} nvc0_channel_t;
+class channel {
+};
 
-
-#ifdef __cplusplus
-}
-#endif
-
+}  // namespace nvc0
 #endif  // HW_NVC0_NVC0_CHANNEL_H_
 /* vim: set sw=4 ts=4 et tw=80 : */
