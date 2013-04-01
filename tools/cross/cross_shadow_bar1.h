d37fffa3dfaf3b8ec383cb214f89852774b67ff1
Yusuke Suzuki <utatane.tea@gmail.com>
2013-04-01 16:32:03 +0900

    Implement Shadow BAR1

---
+++ tools/cross/cross_shadow_bar1.h
@@ -0,0 +1,14 @@
+#ifndef CROSS_SHADOW_BAR1_H_
+#define CROSS_SHADOW_BAR1_H_
+#include "cross.h"
+#include "cross_channel.h"
+namespace cross {
+
+class shadow_bar1 : public channel {
+ public:
+    shadow_bar1();
+};
+
+}  // namespace cross
+#endif  // CROSS_SHADOW_BAR1_H_
+/* vim: set sw=4 ts=4 et tw=80 : */
