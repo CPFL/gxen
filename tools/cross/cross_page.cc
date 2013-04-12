61a30f25999d6492d8d1f51295ccd2b581aa0f96
Yusuke Suzuki <utatane.tea@gmail.com>
2013-04-12 19:28:04 +0900

    Fix page size APIs

--- tools/cross/cross_page.cc
+++ tools/cross/cross_page.cc
@@ -43,23 +43,27 @@ page::~page() {
 
 void page::clear() {
     pramin::accessor pramin;
-    for (std::size_t i = 0; i < ((0x1000 * size()) / sizeof(uint32_t)); i += sizeof(uint32_t)) {
+    for (std::size_t i = 0; i < (size() / sizeof(uint32_t)); i += sizeof(uint32_t)) {
         pramin.write32(address() + i, 0);
     }
 }
 
 void page::write32(uint64_t offset, uint32_t value) {
-    assert(offset < (0x1000 * size()));
+    assert(offset < size());
     pramin::write32(address() + offset, value);
 }
 
 uint32_t page::read32(uint64_t offset) {
-    assert(offset < (0x1000 * size()));
+    assert(offset < size());
     return pramin::read32(address() + offset);
 }
 
-std::size_t page::size() const {
+std::size_t page::page_size() const {
     return vram_->n();
+}
+
+uint64_t page::size() const {
+    return page_size() * 0x1000ULL;
 }
 
 }  // namespace cross
