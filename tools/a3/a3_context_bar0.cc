5b5df7343d10c436202628891a8cbcfcdf53179a
Yusuke Suzuki <utatane.tea@gmail.com>
2013-05-17 13:12:53 +0900

    We cannot remove barriers on instmem
    Because of ramin initialization

--- tools/a3/a3_context_bar0.cc
+++ tools/a3/a3_context_bar0.cc
@@ -161,6 +161,8 @@ void context::write_bar0(const command& cmd) {
                         for (iter_t it = range.first; it != range.second; ++it) {
                             const uint32_t res = bit_clear<28>(data) | (it->second->shadow_ramin()->address() >> 12);
                             A3_LOG("    channel %d ramin graph with cmd %" PRIX32 " with addr %" PRIX64 "\n", it->second->id(), cmd.value, it->second->shadow_ramin()->address());
+                            // Because we doesn't recognize PCOPY engine initialization
+                            // it->second->shadow(this);
                             registers::write32(0x409500, res);
                             registers::write32(0x409504, cmd.value);
                         }
