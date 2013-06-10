edc0003612ca88acee56083695515d0aad3be4f0
Yusuke Suzuki <utatane.tea@gmail.com>
2013-06-10 16:09:02 +0900

    Add timer

---
+++ tools/a3/a3_timer.cc
@@ -0,0 +1,62 @@
+/*
+ * A3 timer
+ *
+ * Copyright (c) 2012-2013 Yusuke Suzuki
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include <stdint.h>
+#include "a3.h"
+#include "a3_timer.h"
+#include "a3_context.h"
+namespace a3 {
+
+timer_t::timer_t(const boost::posix_time::time_duration& wait)
+    : wait_(wait)
+    , thread_()
+{
+}
+
+timer_t::~timer_t() {
+    stop();
+}
+
+void timer_t::start() {
+    if (thread_) {
+        stop();
+    }
+    thread_.reset(new boost::thread(&timer_t::run, this));
+}
+
+void timer_t::stop() {
+    if (thread_) {
+        thread_->interrupt();
+        thread_.reset();
+    }
+}
+
+void timer_t::run() {
+    while (true) {
+        boost::this_thread::yield();
+        boost::this_thread::sleep(wait_);
+    }
+}
+
+}  // namespace a3
+/* vim: set sw=4 ts=4 et tw=80 : */
--- tools/a3/a3_timer.h
+++ tools/a3/a3_timer.h
@@ -1,12 +1,26 @@
 #ifndef A3_TIMER_H_
 #define A3_TIMER_H_
 #include <boost/noncopyable.hpp>
+#include <boost/thread.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
 #include "a3.h"
 namespace a3 {
 
 class context;
 
-class timer : boost::noncopyable {
+class timer_t : private boost::noncopyable {
+ public:
+    timer_t(const boost::posix_time::time_duration& wait);
+    ~timer_t();
+    void start();
+    void stop();
+
+ private:
+    void run();
+
+    boost::posix_time::time_duration wait_;
+    boost::scoped_ptr<boost::thread> thread_;
 };
 
 }  // namespace a3
