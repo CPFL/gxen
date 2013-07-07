9f5e527ab3ac0423bc1b4782e0789deaa160d9fa
Yusuke Suzuki <utatane.tea@gmail.com>
2013-07-07 10:51:23 +0900

    Add a3 BAND scheduler template

---
+++ tools/a3/a3_band_scheduler.h
@@ -0,0 +1,37 @@
+#ifndef A3_BAND_SCHEDULER_H_
+#define A3_BAND_SCHEDULER_H_
+#include <queue>
+#include <boost/noncopyable.hpp>
+#include <boost/thread.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+#include "a3.h"
+#include "a3_lock.h"
+namespace a3 {
+
+class context;
+class device;
+
+class band_scheduler : private boost::noncopyable {
+ public:
+    friend class device;
+
+    band_scheduler(const boost::posix_time::time_duration& wait);
+    ~band_scheduler();
+    void start();
+    void stop();
+
+ private:
+    typedef std::pair<context*, command> fire_t;
+    void run();
+    void enqueue(context* ctx, const command& cmd);
+
+    boost::posix_time::time_duration wait_;
+    boost::scoped_ptr<boost::thread> thread_;
+    mutex mutex_;
+    std::queue<fire_t> queue_;
+};
+
+}  // namespace a3
+#endif  // A3_BAND_SCHEDULER_H_
+/* vim: set sw=4 ts=4 et tw=80 : */
