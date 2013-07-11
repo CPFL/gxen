4fa894d61e315047e4cad21109de9386c6828a94
Yusuke Suzuki <utatane.tea@gmail.com>
2013-07-12 04:05:26 +0900

    Use a3.h BAR3 size

--- tools/a3/a3.h
+++ tools/a3/a3.h
@@ -13,6 +13,8 @@ namespace a3 {
 #define A3_1G 0x40000000ULL
 #define A3_2G (A3_1G * 2)
 
+#define A3_VM_NUM 8
+
 #define A3_MEMORY_CTL_NUM 1
 #define A3_MEMORY_CTL_PART (A3_1G / 2)
 #define A3_MEMORY_SIZE (A3_MEMORY_CTL_PART * A3_MEMORY_CTL_NUM)
@@ -21,6 +23,10 @@ namespace a3 {
 #define A3_BAR0_SIZE (32ULL << 20)
 #define A3_BAR4_SIZE (0x1000ULL)
 #define A3_GUEST_DATA_SIZE (0x1000ULL * 4)
+
+// Because BAR3 effective area is limited to 16MB
+#define A3_BAR3_ARENA_SIZE ((1ULL << 20) * 2)
+#define A3_BAR3_TOTAL_SIZE (A3_BAR3_ARENA_SIZE * A3_VM_NUM)
 
 #if defined(NDEBUG)
     #define A3_FPRINTF(stream, fmt, args...) do { } while (0)
