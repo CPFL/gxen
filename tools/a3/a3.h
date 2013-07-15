9b45e1a062a0357fc1adb851ef4660c7dd7ba4c1
Yusuke Suzuki <utatane.tea@gmail.com>
2013-07-15 09:07:21 +0900

    Extract to a3_config.h

--- tools/a3/a3.h
+++ tools/a3/a3.h
@@ -4,30 +4,8 @@
 #include <cassert>
 #include <stdint.h>
 #include <boost/interprocess/ipc/message_queue.hpp>
+#include "a3_config.h"
 namespace a3 {
-
-#define A3_VERSION "0.0.1"
-#define A3_ENDPOINT "/tmp/a3_endpoint"
-#define A3_CHANNELS 128
-#define A3_1G 0x40000000ULL
-#define A3_2G (A3_1G * 2)
-
-#define A3_VM_NUM 8
-
-#define A3_DOMAIN_CHANNELS (A3_CHANNELS / A3_VM_NUM)
-
-#define A3_MEMORY_CTL_NUM 1
-#define A3_MEMORY_CTL_PART (A3_1G / 2)
-#define A3_MEMORY_SIZE (A3_MEMORY_CTL_PART * A3_MEMORY_CTL_NUM)
-
-#define A3_GPC_BCAST(r) (0x418000 + (r))
-#define A3_BAR0_SIZE (32ULL << 20)
-#define A3_BAR4_SIZE (0x1000ULL)
-#define A3_GUEST_DATA_SIZE (0x1000ULL * 4)
-
-// Because BAR3 effective area is limited to 16MB
-#define A3_BAR3_TOTAL_SIZE (16 * (1ULL << 20))
-#define A3_BAR3_ARENA_SIZE (A3_BAR3_TOTAL_SIZE / A3_VM_NUM)
 
 #if defined(NDEBUG)
     #define A3_FPRINTF(stream, fmt, args...) do { } while (0)
