ef341c230001171c00aed7c2b830e94013aa48d3
Yusuke Suzuki <utatane.tea@gmail.com>
2013-07-18 21:34:01 +0900

    Set VM nums

--- tools/a3/a3_config.h
+++ tools/a3/a3_config.h
@@ -7,12 +7,12 @@
 #define A3_1G 0x40000000ULL
 #define A3_2G (A3_1G * 2)
 
-#define A3_VM_NUM 8
+#define A3_VM_NUM 2
 
 #define A3_DOMAIN_CHANNELS (A3_CHANNELS / A3_VM_NUM)
 
-#define A3_MEMORY_CTL_NUM 1
 #define A3_MEMORY_CTL_PART (A3_1G / 2)
+#define A3_MEMORY_CTL_NUM (((A3_2G * 2) / A3_MEMORY_CTL_PART) / A3_VM_NUM)
 #define A3_MEMORY_SIZE (A3_MEMORY_CTL_PART * A3_MEMORY_CTL_NUM)
 
 #define A3_GPC_BCAST(r) (0x418000 + (r))
