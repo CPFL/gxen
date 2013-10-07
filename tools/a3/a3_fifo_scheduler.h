71ffe359a16478b589b2e604a3daeff08b4d2322
Yusuke Suzuki <utatane.tea@gmail.com>
2013-10-07 11:59:30 +0900

    Add sampling to FIFO

--- tools/a3/a3_fifo_scheduler.h
+++ tools/a3/a3_fifo_scheduler.h
@@ -17,7 +17,7 @@ class fifo_scheduler_t : public scheduler_t {
  public:
     typedef boost::intrusive::list<context> contexts_t;
 
-    fifo_scheduler_t(const boost::posix_time::time_duration& wait, const boost::posix_time::time_duration& period);
+    fifo_scheduler_t(const boost::posix_time::time_duration& wait, const boost::posix_time::time_duration& period, const boost::posix_time::time_duration& sample);
     virtual ~fifo_scheduler_t();
     virtual void start();
     virtual void stop();
@@ -29,17 +29,22 @@ class fifo_scheduler_t : public scheduler_t {
     typedef std::pair<context*, command> fire_t;
     void run();
     void replenish();
+    void sampling();
 
     boost::posix_time::time_duration wait_;
     boost::posix_time::time_duration period_;
+    boost::posix_time::time_duration sample_;
+    boost::posix_time::time_duration gpu_idle_;
     boost::scoped_ptr<boost::thread> thread_;
     boost::scoped_ptr<boost::thread> replenisher_;
+    boost::scoped_ptr<boost::thread> sampler_;
     boost::mutex mutex_;
     boost::condition_variable cond_;
     std::queue<fire_t> queue_;
     timer_t utilization_;
     contexts_t contexts_;
     boost::posix_time::time_duration bandwidth_;
+    boost::posix_time::time_duration sampling_bandwidth_;
 };
 
 }  // namespace a3
