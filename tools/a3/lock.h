42ae4df9f8244efdb304b5d5d11591aa3cf27912
Yusuke Suzuki <utatane.tea@gmail.com>
2013-11-27 20:55:40 +0900

    Make lock compatible for all boost mutex

--- tools/a3/lock.h
+++ tools/a3/lock.h
@@ -1,13 +1,14 @@
 #ifndef A3_LOCK_H_
 #define A3_LOCK_H_
+#include <type_traits>
 #include <boost/thread.hpp>
 namespace a3 {
 
 typedef boost::recursive_mutex mutex_t;
 
 #define A3_SYNCHRONIZED(m) \
-    if (mutex_t::scoped_lock __LOCK__ = mutex_t::scoped_lock(m))
+    if (auto __LOCK__ = boost::unique_lock<std::decay<decltype(m)>::type>(m))
 
 }  // namespace a3
-#endif  // A3_H_
+#endif  // A3_LOCK_H_
 /* vim: set sw=4 ts=4 et tw=80 : */
